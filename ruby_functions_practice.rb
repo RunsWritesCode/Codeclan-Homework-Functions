c485b8160edf4516c36453d3eb652ebd4dfce43b
RunsWritesCode <sophieamullins@gmail.com>
2017-10-31 16:13:15 +0000

    4 tests passed

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,3 +1,22 @@
-def test_return_10()
+def return_10()
   return 10
 end
+
+def add(number_1, number_2)
+  number_1 = 1
+  number_2 = 2
+  return number_1 + number_2
+end
+
+def subtract(number_1, number_2)
+  number_1 = 7
+  number_2 = 2
+  return number_1 - number_2
+end
+
+
+def multiply(number_1, number_2)
+  number_1 = 4
+  number_2 = 2
+  return number_1 * number_2
+  end
